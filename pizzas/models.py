c64b493c06d09673299ba9eb077b65643ce1fbf1
Mason Staatz <mason_staatz1@baylor.edu>
2022-05-06 21:00:04 -0500

    final edits

--- pizzas/models.py
+++ pizzas/models.py
@@ -3,10 +3,13 @@ from django.db import models
 from this import d
 from audioop import maxpp
 from django.contrib.auth.models import User
+from pizzeria.settings import MEDIA_URL
+
 
 # Create your models here.
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=200)
+    pizza_pics = models.ImageField(upload_to='img', blank= True, null= True)
 
     def __str__(self):
         return self.pizza_name
@@ -20,10 +23,8 @@ class Topping(models.Model):
         return self.topping_name
 
 class Comments(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     comment = models.CharField(max_length=250)
     
-
-
     def __str__(self):
         return self.comment
--- pizzas/views.py
+++ pizzas/views.py
@@ -4,20 +4,23 @@ from pizzas.models import Comments, Pizza, Topping, Comments
 from pizzas.forms import CommentsForm
 
 
+
 # Create your views here.
 def index(request):
     return render(request, 'pizzas/index.html')
 
 def pizza_type(request,pizza_id):       #2 types of requests in HTML, get and post. "Get" views the database and pulls info                                     #"Post" adds new info into the database
-    pizza = Pizza.objects.get(id= pizza_id)
+    pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.topping_set.all()
-    context = {'toppings':toppings}
+    picture = Pizza.picture
+    context = {'toppings':toppings, 'picture':picture}
     
-    return render(request, 'pizzas/pizza_type.html', context)
+    
+    return render(request, 'pizzas/pizza_type.html',  context)
 
 
 def pizza_menu(request):        #2 types of requests in HTML, get and post. "Get" views the database and pulls info
-    pizzas = Pizza.objects.filter().order_by()                                        #"Post" adds new info into the database
+    pizzas = Pizza.objects.all()                                      #"Post" adds new info into the database
 
     context = {'pizzas':pizzas}
     
@@ -25,7 +28,8 @@ def pizza_menu(request):        #2 types of requests in HTML, get and post. "Get
 
 
 
-def new_comment(request):
+def new_comment(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
 
     if request.method != 'POST':
         form = CommentsForm()
@@ -34,8 +38,11 @@ def new_comment(request):
         form = CommentsForm(data=request.POST)
 
         if form.is_valid():
-            new_comment = form.save()
-            return redirect('pizzas:pizza_menu')
+            comment = form.save(commit=False)
+            comment.pizza = pizza
+            comment.save()
+            return redirect('pizzas:pizza_menu', pizza_id=pizza_id)
 
-    context = {'form':form}
+    context = {'form':form, 'pizza':pizza}
     return render(request, 'pizzas/new_comment.html', context)
+
